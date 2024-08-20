785c2f90c46da589f3727f0bdf72f9beb66881be
Ezequiel Martin Oyhambure <ezequiel@oyhambure.com>
2024-08-20 00:07:02 -0300

    Finished two_sum and changed Vec<i32> to slices

--- tests/AreaOfArray.rs
+++ tests/AreaOfArray.rs
@@ -4,10 +4,10 @@ mod test_area_of_array{
 
     #[test]
     fn test_example1(){
-        assert_eq!((12, (1, 4)), calculate_area_of_array(vec![1, 5, 6, 3, 4]))
+        assert_eq!((12, (1, 4)), calculate_area_of_array(&[1, 5, 6, 3, 4]))
     }
     #[test]
     fn test_example2(){
-        assert_eq!((35, (0, 5)), calculate_area_of_array(vec![10, 6, 5, 6, 5, 7]))
+        assert_eq!((35, (0, 5)), calculate_area_of_array(&[10, 6, 5, 6, 5, 7]))
     }
 }
--- tests/MonotonicArray.rs
+++ tests/MonotonicArray.rs
@@ -3,30 +3,30 @@ mod monotonic_array_tests {
     use dsa::*;
     #[test]
     fn test_basic_increasing(){
-        assert_eq!(true, monotonic_array(vec![1, 2, 3, 4]));
+        assert!(monotonic_array(&[1, 2, 3, 4]));
     }
     #[test]
     fn test_basic_decreasing(){
-        assert_eq!(true, monotonic_array(vec![4, 3, 2, 1]));
+        assert!(monotonic_array(&[4, 3, 2, 1]));
     }
     #[test]
     fn test_negative_increasing(){
-        assert_eq!(true, monotonic_array(vec![-4, -3, -2, -1]));
+        assert!(monotonic_array(&[-4, -3, -2, -1]));
     }
     #[test]
     fn test_negative_decreasing(){
-        assert_eq!(true, monotonic_array(vec![-1, -2, -3, -4]));
+        assert!(monotonic_array(&[-1, -2, -3, -4]));
     }
     #[test]
     fn test_mixed(){
-        assert_eq!(true, monotonic_array(vec![-4, -3, 0, 1, 2, 3]));
+        assert!(monotonic_array(&[-4, -3, 0, 1, 2, 3]));
     }
     #[test]
     fn test_zero(){
-        assert_eq!(true, monotonic_array(vec![0]));
+        assert!(monotonic_array(&[0]));
     }
     #[test]
     fn test_empty(){
-        assert_eq!(true, monotonic_array(vec![]));
+        assert!(monotonic_array(&[]));
     }
 }
